f6e016289d7a804fd2fab7330887fa2be94ad5b7
gajkez <125378585+LukaGajic2@users.noreply.github.com>
2023-04-27 17:57:44 +0200

    Tidy up script.js

--- assets/javascript/script.js
+++ assets/javascript/script.js
@@ -44,50 +44,51 @@ function computerChoice() {
   let computerResult = '';
 
   if (randomeNumber >= 0 && randomeNumber < 1/5) {
-		computerResult = 'rock';
-	}else if (randomeNumber >= 1/5 && randomeNumber < 2/5) {
+    computerResult = 'rock';
+  }else if (randomeNumber >= 1/5 && randomeNumber < 2/5) {
     computerResult = 'paper';
   }else if (randomeNumber >= 2/5 && randomeNumber < 3/5) {
     computerResult = 'scissors';
   }else if (randomeNumber >= 3/5 && randomeNumber < 4/5) {
-		computerResult = 'lizard';
+    computerResult = 'lizard';
   }else if (randomeNumber >= 4/5 && randomeNumber < 5) {
     computerResult = 'spock';
   }
-	return computerResult;
+  return computerResult;
 }
 
 function playerGame(playerResult) {
 	const computerResult = computerChoice();
   let result = '';
-
-  if (playerResult === 'rock') {
+	
+	if (playerResult === 'rock') {
 		if (computerResult === 'rock') {
-	  result = 'Tie';
-		}else if (computerResult === 'paper') {
+      result = 'Tie';
+    }else if (computerResult === 'paper') {
       result = 'You Lose!';
-  	}else if (computerResult === 'scissors') {
+    }else if (computerResult === 'scissors') {
       result = 'You Win!';
     }else if (computerResult === 'lizard') {
       result = 'You Win!';
     }else if (computerResult === 'spock') {
       result = 'You Lose!';
     }
-
-	}else if (playerResult === 'paper') {
-	  if (computerResult === 'rock') {
-		result = 'You Lose!';
-      }else if (computerResult === 'paper') {
-       	result = 'Tie';
-      }else if (computerResult === 'scissors') {
-        result = 'You Lose!';
-      }else if (computerResult === 'lizard') {
-        result = 'You Lose!';
-      }else if (computerResult === 'spock') {
-        result = 'You Win!';
-      }
-    }else if (playerResult === 'scissors') {
-      if (computerResult === 'rock') {
+	  
+  }else if (playerResult === 'paper') {
+    if (computerResult === 'rock') {
+      result = 'You Lose!';
+    }else if (computerResult === 'paper') {
+      result = 'Tie';
+    }else if (computerResult === 'scissors') {
+      result = 'You Lose!';
+    }else if (computerResult === 'lizard') {
+      result = 'You Lose!';
+    }else if (computerResult === 'spock') {
+      result = 'You Win!';
+    }
+	  
+  }else if (playerResult === 'scissors') {
+    if (computerResult === 'rock') {
         result = 'You Lose!';
       }else if (computerResult === 'paper') {
         result = 'You Win!';
